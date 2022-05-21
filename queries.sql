d805c543714980479137ca8e3c5d3b5e6f5e6d55
Ricardo Guerreiro <102628808+ricardoguerreiro92@users.noreply.github.com>
2022-05-21 18:15:31 -0400

    Add queries performed during module

--- queries.sql
+++ queries.sql
@@ -24,7 +24,106 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+-- Check retirement_info table
 SELECT * FROM retirement_info;
+
+SELECT first_name, last_name, title
+FROM employees AS e
+LEFT JOIN titles AS t ON e.emp_no = t.emp_no
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees with emp number
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no;
+
+-- Joining retirement_info and dept_emp tables creating a table with who is eligible for retirement and is still employeed
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+--Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- creates emp_info table to add gender to retirement_info table -- This will only be a temporary table
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS S
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	 
+SELECT * FROM emp_info;
+
+--List of managers retir per department
+SELECT dm.dept_no,
+		d.dept_name,
+		dm.emp_no,
+		ce.last_name,
+		ce.first_name,
+		dm.from_date,
+		dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON(dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		on(dm.emp_no = ce.emp_no);
+
+SELECT * FROM manager_info;
+
+--List of employees per department
+SELECT ce.emp_no, ce.first_name, ce.last_name, d.dept_name
+--INTO dept_info
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (d.dept_no = de.dept_no);
 
 --SkillDrill 7.3.1
 SELECT first_name, last_name
@@ -42,3 +141,39 @@ WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- SkillDrill 7.3.4 - Creates new table retirement_info_per_dept to store how many people are retirement eligible per department
+-- Also shows Department Name w/ 3rd JOIN
+SELECT COUNT(ce.emp_no), de.dept_no, d.dept_name
+INTO retirement_info_per_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+GROUP BY de.dept_no, d.dept_name
+ORDER BY de.dept_no;
+
+SELECT * FROM retirement_info_per_dept;
+
+--SkillDrill 7.3.6 - Create a query that will return only the information relevant to the Sales team
+SELECT ri.emp_no, ri.last_name, ri.first_name, d.dept_name
+INTO sales_dept
+FROM retirement_info AS ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (d.dept_no = de.dept_no)
+WHERE (d.dept_name = 'Sales');
+
+--SkillDrill 7.3.6 - Adding Development team
+
+SELECT ri.emp_no, ri.last_name, ri.first_name, d.dept_name
+INTO sales_dev_dept
+FROM retirement_info AS ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (d.dept_no = de.dept_no)
+WHERE (d.dept_name IN ('Sales','Development'))
+ORDER BY d.dept_name;
